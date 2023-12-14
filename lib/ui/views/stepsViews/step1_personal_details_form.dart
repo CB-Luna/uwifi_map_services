b70269573d232de10c7860cc0b9d7190d1d83d7c
Uzziel Palma <uzzielpalma99@gmail.com>
2023-12-14 17:37:42 -0600

    Se cambia var nombreCharacters a nameCharacters

--- lib/ui/views/stepsViews/step1_personal_details_form.dart
+++ lib/ui/views/stepsViews/step1_personal_details_form.dart
@@ -23,7 +23,7 @@ class _Step1PersonalDetailsFormState extends State<Step1PersonalDetailsForm> {
   @override
   Widget build(BuildContext context) {
     final customerPDSDCCController = Provider.of<CustomerPDSDCCProvider>(context);
-    final nombreCharacters = RegExp(r'^(([A-Z]{1}|[ÁÉÍÓÚÑ]{1})[a-zá-ÿ]+[ ]?)+$');
+    final nameCharacters = RegExp(r'^(([A-Z]{1}|[ÁÉÍÓÚÑ]{1})[a-zá-ÿ]+[ ]?)+$');
     final phoneCharacters = RegExp(r'^[0-9\-() ]+$');
     // final zcode = widget.zipcode.toString();
     var phoneFormat = MaskTextInputFormatter(
@@ -126,7 +126,7 @@ class _Step1PersonalDetailsFormState extends State<Step1PersonalDetailsForm> {
                                               maxHeight: 55),
 
                                     validator: (value) {
-                                        return (nombreCharacters.hasMatch(value ?? ''))
+                                        return (nameCharacters.hasMatch(value ?? ''))
                                         ? null
                                         : 'Please enter your name, the name should be capitalized.';
                                     },
@@ -156,7 +156,7 @@ class _Step1PersonalDetailsFormState extends State<Step1PersonalDetailsForm> {
                                               maxHeight: 55),
 
                                      validator: (value) {
-                                        return (nombreCharacters.hasMatch(value ?? ''))
+                                        return (nameCharacters.hasMatch(value ?? ''))
                                         ? null
                                         : 'Please enter your last name, the last name should be capitalized.';
                                     },
